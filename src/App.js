191062cdfac4ee802bebb50e6d4a013a0698ed4b
salvatore <salvatore.la.spata@gmail.com>
2021-05-17 15:33:56 +0200

    il mistero si infittisce

--- src/App.js
+++ src/App.js
@@ -1,61 +1,67 @@
 import './App.css';
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { CssBaseline } from "@material-ui/core";
-import { ThemeProvider } from "@material-ui/core/styles";
+import { CssBaseline, useTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
 
-import { useStyles } from "./components/hook/useStyles";
-import { useDarkMode } from "./components/hook/useDarkMode";
+import { useStyles } from './components/hook/useStyles';
+import { useDarkMode } from './components/hook/useDarkMode';
 
-import Bar from "./components/Bar/Bar";
-import DrawerMenu from "./components/DrawerMenu/DrawerMenu";
+import Bar from './components/Bar/Bar';
+import DrawerMenu from './components/DrawerMenu/DrawerMenu';
 
-import { ITEMS_MENU } from "./config/constants";
-import { useDrawer } from "./components/hook/useDrawer";
-
+import { ITEMS_MENU } from './config/constants';
+import { useDrawer } from './components/hook/useDrawer';
 
 function App() {
-   const classes = useStyles();
-   const { darkState, darkTheme, handleThemeChange } = useDarkMode(true);
-   const {
-      open: isOpenDrawer,
-      handleDrawerOpen,
-      handleDrawerClose,
-   } = useDrawer(false);
+     const theme = useTheme();
+     const classes = useStyles(theme);
+     const { darkState, darkTheme, handleThemeChange } = useDarkMode(true);
+     const {
+          open: isOpenDrawer,
+          handleDrawerOpen,
+          handleDrawerClose,
+     } = useDrawer(true);
 
+     return (
+          <div className={classes.root}>
+               <ThemeProvider theme={darkTheme}>
+                    <CssBaseline />
+                    <Bar
+                         open={isOpenDrawer}
+                         handleDrawerOpen={handleDrawerOpen}
+                         darkState={darkState}
+                         handleThemeChange={handleThemeChange}
+                    />
+                    <Router>
+                         <DrawerMenu
+                              open={isOpenDrawer}
+                              handleDrawerClose={handleDrawerClose}
+                         />
 
-  return (
-    <div className={classes.root}>
-         <ThemeProvider theme={darkTheme}>
-            <CssBaseline />
-            <Bar
-               open={isOpenDrawer}
-               handleDrawerOpen={handleDrawerOpen}
-               darkState={darkState}
-               handleThemeChange={handleThemeChange}
-            />
-            <Router>
-               <DrawerMenu
-                  open={isOpenDrawer}
-                  handleDrawerClose={handleDrawerClose}
-               />
-
-               <main className={classes.content}>
-                  <div className={classes.appBarSpacer} />
-                  {/* ROUTING */}
-                  <Switch>
-                     {ITEMS_MENU(darkState, handleThemeChange).map((route) => (
-                        <Route key={route.id} exact path={route.path}>
-                           {route.component}
-                        </Route>
-                     ))}
-                  </Switch>
-               </main>
-            </Router>
-         </ThemeProvider>
-      </div>
-  );
+                         <main className={classes.content}>
+                              <div className={classes.appBarSpacer} />
+                              {/* ROUTING */}
+                              <Switch>
+                                   {ITEMS_MENU(
+                                        darkState,
+                                        handleThemeChange
+                                   ).map((route) => (
+                                        <Route
+                                             key={route.id}
+                                             exact
+                                             path={route.path}
+                                        >
+                                             {route.component}
+                                        </Route>
+                                   ))}
+                              </Switch>
+                         </main>
+                    </Router>
+               </ThemeProvider>
+          </div>
+     );
 }
 
 export default App;
