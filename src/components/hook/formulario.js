0d18c8e2208e4487b0dc66cc2b578978befead1c
salvatore <salvatore.la.spata@gmail.com>
2021-05-21 17:30:29 +0200

    upp

--- src/components/hook/formulario.js
+++ src/components/hook/formulario.js
@@ -1,38 +1,42 @@
 export const formulario = (l) => (a) => (b) => (h) => {
-     const telaioB = b,
-          telaioH = h;
-     let antaB = 0,
-          antaH = 0;
-     let fascioneB = 0;
-     let tAScattoH = 0;
-     if (a === 1) {
-          if (l === 3) {
-               antaH = telaioH - 22;
-               tAScattoH = antaH - 37;
-          }
-          if (l === 4) {
-               antaH = telaioH - 30;
-               tAScattoH = antaH - 74;
-          }
-          antaB = telaioB - 30;
-          fascioneB = antaB - 132;
-     }
-     if (a === 2) {
-          if (l === 3) {
-               antaH = telaioH - 22;
-               tAScattoH = antaH - 37;
-          }
-          if (l === 4) {
-               antaH = telaioH - 30;
-               tAScattoH = antaH - 74;
-          }
-          antaB = (telaioB - 35) / 2;
-          fascioneB = antaB - 132;
-     }
-     return [
-          { tipo: 'TELAIO', base: telaioB, altezza: telaioH },
-          { tipo: 'ANTA', base: antaB, altezza: antaH },
-          { tipo: 'FASCIONE', base: fascioneB, altezza: '' },
-          { tipo: 'T A SCATTO', base: '', altezza: tAScattoH },
-     ];
+    const telaioB = b,
+        telaioH = h;
+    let antaB = 0,
+        antaH = 0,
+        fascione = 0,
+        tAScatto = 0,
+        mezzaLamella = 0,
+        lamella = 0;
+    if (a === 1) {
+        if (l === 3) {
+            antaH = telaioH - 22;
+            tAScatto = antaH - 37;
+        }
+        if (l === 4) {
+            antaH = telaioH - 30;
+            tAScatto = antaH - 74;
+        }
+        antaB = telaioB - 30;
+        fascione = antaB - 132;
+    }
+    if (a === 2) {
+        if (l === 3) {
+            antaH = telaioH - 22;
+            tAScatto = antaH - 37;
+        }
+        if (l === 4) {
+            antaH = telaioH - 30;
+            tAScatto = antaH - 74;
+        }
+        antaB = (telaioB - 35) / 2;
+        fascione = antaB - 132;
+    }
+    return [
+        { tipo: 'TELAIO', base: telaioB, altezza: telaioH },
+        { tipo: 'ANTA', base: antaB, altezza: antaH },
+        { tipo: 'FASCIONE', base: fascione, altezza: '' },
+        { tipo: 'T A SCATTO', base: '', altezza: tAScatto },
+        { tipo: 'Mezza Lamella', base: mezzaLamella, altezza: '' },
+        { tipo: 'Lamella', base: lamella, altezza: '' },
+    ];
 };
--- src/pages/Formulario.jsx
+++ src/pages/Formulario.jsx
@@ -1,116 +1,121 @@
+import React from 'react';
 import {
-     Grid,
-     Paper,
-     TextField,
-     Typography,
-     useTheme,
+    Grid,
+    Paper,
+    TextField,
+    Typography,
+    useTheme,
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
 } from '@material-ui/core';
 import clsx from 'clsx';
-import React from 'react';
 import { useStyles } from '../components/hook/useStyles';
 import StandardContainer from '../components/layout/StandardContainer';
 import TableFormulario from '../components/TableFormulario/TableFormulario';
 import { formulario } from '../components/hook/formulario';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const rowsTelaio3Aula1 = formulario(3)(1);
 const rowsTelaio3Aula2 = formulario(3)(2);
 const rowsTelaio4Aula1 = formulario(4)(1);
 const rowsTelaio4Aula2 = formulario(4)(2);
 
-//si andranno a gestire tutti i flussi di anagrafica
 const Formulario = () => {
-     const theme = useTheme();
-     const classes = useStyles(theme);
+    const theme = useTheme();
+    const classes = useStyles(theme);
 
-     const [base, setBase] = React.useState(1000);
-     const [altezza, setAltezza] = React.useState(1000);
+    const [base, setBase] = React.useState(1000);
+    const [altezza, setAltezza] = React.useState(1000);
 
-     return (
-          <>
-               <StandardContainer>
-                    <Grid item xs={12} md={12} lg={12}>
-                         <Paper className={clsx(classes.paper)}>
-                              <Grid
-                                   container
-                                   direction='row'
-                                   justify='space-around'
-                              >
-                                   <form noValidate autoComplete='off'>
-                                        <TextField
-                                             label='Base'
-                                             variant='outlined'
-                                             onChange={(e) =>
-                                                  setBase(e.target.value)
-                                             }
-                                             value={base}
-                                        />
-                                        <TextField
-                                             label='Altezza'
-                                             variant='outlined'
-                                             onChange={(e) =>
-                                                  setAltezza(e.target.value)
-                                             }
-                                             value={altezza}
-                                        />
-                                   </form>
-                              </Grid>
-                         </Paper>
-                    </Grid>
-                    <Grid item xs={12} md={12} lg={12}>
-                         <Paper className={clsx(classes.paper)}>
-                              <Typography
-                                   component='h5'
-                                   variant='h5'
-                                   color='inherit'
-                                   noWrap
-                              >
-                                   Telaio su 3 lati
-                              </Typography>
-                              <Grid
-                                   container
-                                   direction='row'
-                                   justify='space-around'
-                              >
-                                   <TableFormulario
-                                        rows={rowsTelaio3Aula1(base)(altezza)}
-                                        ante={1}
-                                   />
-                                   <TableFormulario
-                                        rows={rowsTelaio3Aula2(base)(altezza)}
-                                        ante={2}
-                                   />
-                              </Grid>
-                         </Paper>
-                    </Grid>
-                    <Grid item xs={12} md={12} lg={12}>
-                         <Paper className={clsx(classes.paper)}>
-                              <Typography
-                                   component='h5'
-                                   variant='h5'
-                                   color='inherit'
-                                   noWrap
-                              >
-                                   Telaio su 4 lati
-                              </Typography>
-                              <Grid
-                                   container
-                                   direction='row'
-                                   justify='space-around'
-                              >
-                                   <TableFormulario
-                                        rows={rowsTelaio4Aula1(base)(altezza)}
-                                        ante={1}
-                                   />
-                                   <TableFormulario
-                                        rows={rowsTelaio4Aula2(base)(altezza)}
-                                        ante={2}
-                                   />
-                              </Grid>
-                         </Paper>
-                    </Grid>
-               </StandardContainer>
-          </>
-     );
+    return (
+        <>
+            <StandardContainer>
+                <Grid item xs={12} md={12} lg={12}>
+                    <Paper className={clsx(classes.paper)}>
+                        <form noValidate autoComplete='off'>
+                            <Grid
+                                container
+                                direction='row'
+                                justify='space-around'
+                            >
+                                <TextField
+                                    label='Base'
+                                    variant='outlined'
+                                    onChange={(e) => setBase(e.target.value)}
+                                    value={base}
+                                />
+                                <TextField
+                                    label='Altezza'
+                                    variant='outlined'
+                                    onChange={(e) => setAltezza(e.target.value)}
+                                    value={altezza}
+                                />
+                            </Grid>
+                        </form>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={12} lg={12}>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls='panel1a-content'
+                            id='panel1a-header'
+                        >
+                            <Typography className={classes.heading}>
+                                Telaio su 3 lati
+                            </Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Grid
+                                container
+                                direction='row'
+                                justify='space-around'
+                            >
+                                <TableFormulario
+                                    rows={rowsTelaio3Aula1(base)(altezza)}
+                                    ante={1}
+                                />
+                                <TableFormulario
+                                    rows={rowsTelaio3Aula2(base)(altezza)}
+                                    ante={2}
+                                />
+                            </Grid>
+                        </AccordionDetails>
+                    </Accordion>
+                </Grid>
+                <Grid item xs={12} md={12} lg={12}>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls='panel1a-content'
+                            id='panel1a-header'
+                        >
+                            <Typography className={classes.heading}>
+                                Telaio su 4 lati
+                            </Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Grid
+                                container
+                                direction='row'
+                                justify='space-around'
+                            >
+                                <TableFormulario
+                                    rows={rowsTelaio4Aula1(base)(altezza)}
+                                    ante={1}
+                                />
+                                <TableFormulario
+                                    rows={rowsTelaio4Aula2(base)(altezza)}
+                                    ante={2}
+                                />
+                            </Grid>
+                        </AccordionDetails>
+                    </Accordion>
+                </Grid>
+            </StandardContainer>
+        </>
+    );
 };
 
 export default Formulario;
